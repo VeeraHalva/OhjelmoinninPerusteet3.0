b0dc64e63b094407c8e49857e03a149cfa5d9e83
Veera Hälvä <veera.halva@centria.fi>
2025-12-10 11:07:14 +0200

    Aivan kamaa koodia

--- Viikko5B/viikot41-43.py
+++ Viikko5B/viikot41-43.py
@@ -416,7 +416,307 @@ def main():
         f.write(Viikko43yht)
         f.write(Kaikkiyht)
 
+#tähän koodi jolla luodaan se saamarin valikko!
+
+def main():
+    """Ohjelman pääfunktio: lukee datan, laskee yhteenvedot ja tulostaa raportin."""
+
+    # Ladataan kaikki viikkotiedot
+    KulutusTuotantoViikko41 = lue_data("viikko41.csv")
+    KulutusTuotantoViikko42 = lue_data("viikko42.csv")
+    KulutusTuotantoViikko43 = lue_data("viikko43.csv")
+#TÄSTÄ ALKAA BONARIT
+
+from datetime import datetime
+ 
+#Uutta koodia
+
+def muunna_tiedot(kulutusTuotanto: list) -> list:
+    #muuttaa jokaisen tietorivin sopivaan tietotyyppiin
+    muutettu_tietorivi = []
+    muutettu_tietorivi.append(datetime.fromisoformat(kulutusTuotanto[0]))
+    muutettu_tietorivi.append(int(kulutusTuotanto[1]))
+    muutettu_tietorivi.append(int(kulutusTuotanto[2]))
+    muutettu_tietorivi.append(int(kulutusTuotanto[3]))
+    muutettu_tietorivi.append(int(kulutusTuotanto[4]))
+    muutettu_tietorivi.append(int(kulutusTuotanto[5]))
+    muutettu_tietorivi.append(int(kulutusTuotanto[6]))
+    return muutettu_tietorivi
+
+def lue_data(tiedoston_nimi: str) -> list:
+    """Luekee tiedoston ja palauta rivit sopivassa rakenteessa ja tietotyypeissä"""
+    kulutusJaTuotantoTiedot = []
+    with open(tiedoston_nimi, "r", encoding="utf-8") as f:
+        next(f) # Ohitetaan otsikkorivi
+        for kulutusTuotantoTieto in f:
+            kulutusTuotantoTieto = kulutusTuotantoTieto.strip()
+            kulutusTuotantoTietoSarakkeet = kulutusTuotantoTieto.split(';')
+            kulutusJaTuotantoTiedot.append(muunna_tiedot(kulutusTuotantoTietoSarakkeet))
+
+    return kulutusJaTuotantoTiedot
+
+def paivantiedot(paivamaara: str, lukemat: list) -> int:
+    pv = int(paivamaara.split('.')[0])
+    kk = int(paivamaara.split('.')[1])
+    vuosi = int(paivamaara.split('.')[2])
+    lasketutTiedot = [] 
+    kulutus1vaihe = 0   
+    kulutus2vaihe = 0
+    kulutus3vaihe = 0
+    tuotanto1vaihe = 0
+    tuotanto2vaihe = 0  
+    tuotanto3vaihe = 0
+    for lukema in lukemat:
+        if lukema[0].date() == datetime(vuosi, kk, pv).date():
+            kulutus1vaihe += lukema[1]   
+            kulutus2vaihe += lukema[2]
+            kulutus3vaihe += lukema[3]
+            tuotanto1vaihe += lukema[4]
+            tuotanto2vaihe += lukema[5]  
+            tuotanto3vaihe += lukema[6]
+
+    lasketutTiedot.append(kulutus1vaihe/1000)
+    lasketutTiedot.append(kulutus2vaihe/1000)
+    lasketutTiedot.append(kulutus3vaihe/1000)
+    lasketutTiedot.append(tuotanto1vaihe/1000)
+    lasketutTiedot.append(tuotanto2vaihe/1000)
+    lasketutTiedot.append(tuotanto3vaihe/1000)
+    return lasketutTiedot
+
+            
+def main():
+    """ohjelman pääfunktio: lukee datan, laskee yhteenvedot ja tulostaa raportin"""
     
+    lukemat1 = lue_data("viikko41.csv")
+    lukemat2 = lue_data("viikko42.csv")
+    lukemat3 = lue_data("viikko43.csv")
+    #print(lue_data("viikko42.csv"))
+    #print(lue_data("viikko42.csv")[0][0])
+    
+    print( )
+    Päivä = "päivä"
+    Pvm = "pvm"
+    Kulutus = "Kulutus [kWh]"
+    Tuotanto = "Tuotanto [kWh]" 
+    pvkkvvvv = "(pv.kk.vvvv)"
+    v1 = "v1"   
+    v2 = "v2"
+    v3 = "v3"
+        
+#päivämäärät voisi varmaan käydä omassa silmukassaan läpi, niin ei tulisi toistoa. Mutta tämä tulostaa jo halutun lopputuloksen.
+#bonus tehtävän tulostus pyritty tekemään silmukoita hyväksi käyttäen. tehty uudestaan, ettei sotke alkuperäistä toimivaa koodia.
+
+    otsikkooo = "Bonus-tehtävät:\n"
+    paivat1 =[
+        ("Maanantai", "06.10.2025"),
+        ("Tiistai", "07.10.2025"),
+        ("Keskiviikko", "08.10.2025"),
+        ("Torstai", "09.10.2025"),
+        ("Perjantai", "10.10.2025"),
+        ("Lauantai", "11.10.2025"),
+        ("Sunnuntai", "12.10.2025"),
+        ("Maanantai", "13.10.2025")]
+    paivat2 =[
+        ("Maanantai", "13.10.2025"),
+        ("Tiistai", "14.10.2025"),
+        ("Keskiviikko", "15.10.2025"),
+        ("Torstai", "16.10.2025"),
+        ("Perjantai", "17.10.2025"),
+        ("Lauantai", "18.10.2025"),
+        ("Sunnuntai", "19.10.2025")]
+    paivat3 =[
+        ("Maanantai", "20.10.2025"),
+        ("Tiistai", "21.10.2025"),
+        ("Keskiviikko", "22.10.2025"),
+        ("Torstai", "23.10.2025"),
+        ("Perjantai", "24.10.2025"),
+        ("Lauantai", "25.10.2025"),
+        ("Sunnuntai", "26.10.2025"), 
+        ]
+    
+# ensin lasketaan kaikki nettokulutukset talteen
+    nettokulutukset = {}
+    lukemat = lukemat1 + lukemat2 + lukemat3
+    paivat = paivat1 + paivat2 + paivat3
+    for nimi, pvm in paivat:
+        arvot = paivantiedot(pvm, lukemat)
+        nettokulutus = (arvot[0]+arvot[1]+arvot[2]) - (arvot[3]+arvot[4]+arvot[5])
+        nettokulutukset[pvm] = nettokulutus
+
+# haetaan suurin nettokulutus (numeroarvo)
+    paras_pvm = max(nettokulutukset, key=nettokulutukset.get)
+# tulosteen taulukko vko 41
+    Bviikko41 = "Viikon 41 sähkönkulutus ja -tuotanto (kWh, vaiheittain)\n"
+    Bviikko41 += " \n"
+    Bviikko41 += f"{Päivä:<12}{Pvm:<12}{Kulutus:>17}{Tuotanto :>24}{'Nettokulutus':>24}\n"
+    Bviikko41 += f"{'':<12}{pvkkvvvv:<12}{v1:>7}{v2:>7}{v3:>6}{v1:>10}{v2:>7}{v3:>7}{'päivittäin':>20}\n"
+    Bviikko41 += "--------------------------------------------------------------------------------------------\n"
+    for nimi, pvm in paivat1:
+        arvot = paivantiedot(pvm, lukemat1)
+        kulutus = [f"{x:.2f}".replace('.', ',') for x in arvot[:3]]
+        tuotanto = [f"{x:.2f}".replace('.', ',') for x in arvot[3:]]
+        nettokulutus = (arvot[0]+arvot[1]+arvot[2]) - (arvot[3]+arvot[4]+arvot[5])
+        nettokulutus_str = f"{nettokulutus:.2f}".replace('.', ',')
+        merkki = "*" if pvm == paras_pvm else ""
+
+        Bviikko41 += (f"{nimi:<12}{pvm:<12}"
+          f"{kulutus[0]:>9}{kulutus[1]:>7}{kulutus[2]:>6}"
+          f"{tuotanto[0]:>10}{tuotanto[1]:>7}{tuotanto[2]:>7}"
+          f"{nettokulutus_str:>15}{merkki:>1}\n")
+        
+    Bviikko41 += "-------------------------------------------------------------------------------------------\n"
+    Bviikko41 += "\n"
+# tulostetaan taulukko vko 42
+    Bviikko42 = "Viikon 42 sähkönkulutus ja -tuotanto (kWh, vaiheittain)\n"
+    Bviikko42 += " \n"
+    Bviikko42 += f"{Päivä:<12}{Pvm:<12}{Kulutus:>17}{Tuotanto :>24}{'Nettokulutus':>24}\n"
+    Bviikko42 += f"{'':<12}{pvkkvvvv:<12}{v1:>7}{v2:>7}{v3:>6}{v1:>10}{v2:>7}{v3:>7}{'päivittäin':>20}\n"
+    Bviikko42 += "--------------------------------------------------------------------------------------------\n"
+    for nimi, pvm in paivat2:
+        arvot = paivantiedot(pvm, lukemat2)
+        kulutus = [f"{x:.2f}".replace('.', ',') for x in arvot[:3]]
+        tuotanto = [f"{x:.2f}".replace('.', ',') for x in arvot[3:]]
+        nettokulutus = (arvot[0]+arvot[1]+arvot[2]) - (arvot[3]+arvot[4]+arvot[5])
+        nettokulutus_str = f"{nettokulutus:.2f}".replace('.', ',')
+        merkki = "*" if pvm == paras_pvm else ""
+
+        Bviikko42 += (f"{nimi:<12}{pvm:<12}"
+          f"{kulutus[0]:>9}{kulutus[1]:>7}{kulutus[2]:>6}"
+          f"{tuotanto[0]:>10}{tuotanto[1]:>7}{tuotanto[2]:>7}"
+          f"{nettokulutus_str:>15}{merkki:>1}\n")
+    Bviikko42 += "-------------------------------------------------------------------------------------------\n"
+    Bviikko42 += "\n"
+#tulostetaan taulukko vko 43
+    Bviikko43 = "Viikon 43 sähkönkulutus ja -tuotanto (kWh, vaiheittain)\n"
+    Bviikko43 += " \n"
+    Bviikko43 += f"{Päivä:<12}{Pvm:<12}{Kulutus:>17}{Tuotanto :>24}{'Nettokulutus':>24}\n"
+    Bviikko43 += f"{'':<12}{pvkkvvvv:<12}{v1:>7}{v2:>7}{v3:>6}{v1:>10}{v2:>7}{v3:>7}{'päivittäin':>20}\n"
+    Bviikko43 += "--------------------------------------------------------------------------------------------\n"
+    Bviikko43 += "\n"
+    for nimi, pvm in paivat3:
+        arvot = paivantiedot(pvm, lukemat3)
+        kulutus = [f"{x:.2f}".replace('.', ',') for x in arvot[:3]]
+        tuotanto = [f"{x:.2f}".replace('.', ',') for x in arvot[3:]]
+        nettokulutus = (arvot[0]+arvot[1]+arvot[2]) - (arvot[3]+arvot[4]+arvot[5])
+        nettokulutus_str = f"{nettokulutus:.2f}".replace('.', ',')
+        merkki = "*" if pvm == paras_pvm else ""  
+
+        Bviikko43 += (f"{nimi:<12}{pvm:<12}"
+          f"{kulutus[0]:>9}{kulutus[1]:>7}{kulutus[2]:>6}"
+          f"{tuotanto[0]:>10}{tuotanto[1]:>7}{tuotanto[2]:>7}"
+          f"{nettokulutus_str:>15}{merkki:>1}\n")
+    Bviikko43 += "-------------------------------------------------------------------------------------------\n"
+
+    #print("viikon 41 yhteenveto:")
+    kokonaiskulutus_vk41_v1 = sum(paivantiedot(pvm, lukemat1)[0] for _, pvm in paivat1)
+    kokonaiskulutus_vk41_v2 = sum(paivantiedot(pvm, lukemat1)[1] for _, pvm in paivat1)
+    kokonaiskulutus_vk41_v3 = sum(paivantiedot(pvm, lukemat1)[2] for _, pvm in paivat1)
+    kokonaisproduktio_vk41_v1 = sum(paivantiedot(pvm, lukemat1)[3] for _, pvm in paivat1)
+    kokonaisproduktio_vk41_v2 = sum(paivantiedot(pvm, lukemat1)[4] for _, pvm in paivat1)
+    kokonaisproduktio_vk41_v3 = sum(paivantiedot(pvm, lukemat1)[5] for _, pvm in paivat1)
+
+    Viikko41yht =("\n")       
+    Viikko41yht += "Viikon 41 yhteenveto (kokonaiskulutus ja -tuotanto vaiheittain)\n"
+    Viikko41yht += F"{'':>30}v1{'':>7}v2{'':>7}v3{'':>6}\n"
+    Viikko41yht += "--------------------------------------------------------------\n"
+    Viikko41yht += "kulutus yhteensä [kWh]" + f"{kokonaiskulutus_vk41_v1:>12.2f}".replace('.',',') + f"{kokonaiskulutus_vk41_v2:>8.2f}".replace('.',',') + f"{kokonaiskulutus_vk41_v3:>8.2f}".replace('.',',') + "\n"
+    Viikko41yht += "tuotanto yhteensä [kWh]" + f"{kokonaisproduktio_vk41_v1:>11.2f}".replace('.',',') + f"{kokonaisproduktio_vk41_v2:>8.2f}".replace('.',',') + f"{kokonaisproduktio_vk41_v3:>8.2f}".replace('.',',') + "\n"
+    Viikko41yht += "--------------------------------------------------------------\n"
+
+    #print("viikon 42 yhteenveto:")
+    kokonaiskulutus_vk42_v1 = sum(paivantiedot(pvm, lukemat2)[0] for _, pvm in paivat2)
+    kokonaiskulutus_vk42_v2 = sum(paivantiedot(pvm, lukemat2)[1] for _, pvm in paivat2) 
+    kokonaiskulutus_vk42_v3 = sum(paivantiedot(pvm, lukemat2)[2] for _, pvm in paivat2)
+    kokonaisproduktio_vk42_v1 = sum(paivantiedot(pvm, lukemat2)[3] for _, pvm in paivat2)
+    kokonaisproduktio_vk42_v2 = sum(paivantiedot(pvm, lukemat2)[4] for _, pvm in paivat2)
+    kokonaisproduktio_vk42_v3 = sum(paivantiedot(pvm, lukemat2)[5] for _, pvm in paivat2)
+
+    Viikko42yht =("\n")      
+    Viikko42yht += "Viikon 42 yhteenveto (kokonaiskulutus ja -tuotanto vaiheittain)\n"
+    Viikko42yht += F"{'':>30}v1{'':>7}v2{'':>7}v3{'':>6}\n"
+    Viikko42yht += "--------------------------------------------------------------\n"
+    Viikko42yht += "kulutus yhteensä [kWh]" + f"{kokonaiskulutus_vk42_v1:>12.2f}".replace('.',',') + f"{kokonaiskulutus_vk42_v2:>8.2f}".replace('.',',') + f"{kokonaiskulutus_vk42_v3:>8.2f}".replace('.',',') + "\n"
+    Viikko42yht += "tuotanto yhteensä [kWh]" + f"{kokonaisproduktio_vk42_v1:>11.2f}".replace('.',',') + f"{kokonaisproduktio_vk42_v2:>8.2f}".replace('.',',') + f"{kokonaisproduktio_vk42_v3:>8.2f}".replace('.',',') + "\n"
+    Viikko42yht += "--------------------------------------------------------------\n"
+
+    #print("viikon 43 yhteenveto:")
+    kokonaiskulutus_vk43_v1 = sum(paivantiedot(pvm, lukemat3)[0] for _, pvm in paivat3)
+    kokonaiskulutus_vk43_v2 = sum(paivantiedot(pvm, lukemat3)[1] for _, pvm in paivat3)
+    kokonaiskulutus_vk43_v3 = sum(paivantiedot(pvm, lukemat3)[2] for _, pvm in paivat3)
+    kokonaisproduktio_vk43_v1 = sum(paivantiedot(pvm, lukemat3)[3] for _, pvm in paivat3)
+    kokonaisproduktio_vk43_v2 = sum(paivantiedot(pvm, lukemat3)[4] for _, pvm in paivat3)
+    kokonaisproduktio_vk43_v3 = sum(paivantiedot(pvm, lukemat3)[5] for _, pvm in paivat3)
+
+    Viikko43yht =("\n")    
+    Viikko43yht += "Viikon 43 yhteenveto (kokonaiskulutus ja -tuotanto vaiheittain)\n"
+    Viikko43yht += F"{'':>30}v1{'':>7}v2{'':>7}v3{'':>6}\n"
+    Viikko43yht += "--------------------------------------------------------------\n"
+    Viikko43yht += "kulutus yhteensä [kWh]" + f"{kokonaiskulutus_vk43_v1:>12.2f}".replace('.',',') + f"{kokonaiskulutus_vk43_v2:>8.2f}".replace('.',',') + f"{kokonaiskulutus_vk43_v3:>8.2f}".replace('.',',') + "\n"
+    Viikko43yht += "tuotanto yhteensä [kWh]" + f"{kokonaisproduktio_vk43_v1:>11.2f}".replace('.',',') + f"{kokonaisproduktio_vk43_v2:>8.2f}".replace('.',',') + f"{kokonaisproduktio_vk43_v3:>8.2f}".replace('.',',') + "\n"
+    Viikko43yht += "--------------------------------------------------------------\n"
+
+    kokonaiskulutus_v1 = sum(paivantiedot(pvm, lukemat)[0] for _, pvm in paivat)
+    kokonaiskulutus_v2 = sum(paivantiedot(pvm, lukemat)[1] for _, pvm in paivat)
+    kokonaiskulutus_v3 = sum(paivantiedot(pvm, lukemat)[2] for _, pvm in paivat)
+    kokonaisproduktio_v1 = sum(paivantiedot(pvm, lukemat)[3] for _, pvm in paivat)
+    kokonaisproduktio_v2 = sum(paivantiedot(pvm, lukemat)[4] for _, pvm in paivat)
+    kokonaisproduktio_v3 = sum(paivantiedot(pvm, lukemat)[5] for _, pvm in paivat)
+
+    Kaikkiyht =("\n")       
+    Kaikkiyht += "Kaikkien viikkojen yhteenveto (kokonaiskulutus ja -tuotanto vaiheittain)\n"
+
+    Kaikkiyht += F"{'':>30}v1{'':>7}v2{'':>7}v3{'':>6}\n"
+    Kaikkiyht += "--------------------------------------------------------------\n"
+    Kaikkiyht += "kulutus yhteensä [kWh]" + f"{kokonaiskulutus_v1:>12.2f}".replace('.',',') + f"{kokonaiskulutus_v2:>8.2f}".replace('.',',') + f"{kokonaiskulutus_v3:>8.2f}".replace('.',',') + "\n"
+    Kaikkiyht += "tuotanto yhteensä [kWh]" + f"{kokonaisproduktio_v1:>11.2f}".replace('.',',') + f"{kokonaisproduktio_v2:>8.2f}".replace('.',',') + f"{kokonaisproduktio_v3:>8.2f}".replace('.',',') + "\n"
+    Kaikkiyht += "--------------------------------------------------------------\n"
+
+    # --- VALIKKO ---
+    print("Valikko:")
+    print("1) Luo raportti kaikista viikoista")
+    print("2) Luo raportti vain yhdestä viikosta")
+    valinta = input("Valitse (1/2): ")
+
+    if valinta == "1":
+        # tänne koodi joka luo kaikki raportit
+        # esim. kirjoitetaan tiedostoon kaikki viikot
+        with open("Yhteenvetobonus.txt", "w", encoding="utf-8") as f:
+            f.write("Raportti kaikista viikoista\n")
+            f.write("...\n")
+            f.write(Kaikkiyht)  
+           
+        print("Raportti kaikista viikoista luotu.")
+
+    elif valinta == "2":
+        viikko = input("Anna viikkonumero (41/42/43): ")
+        if viikko == "41":
+            # kirjoita vain viikko41
+            with open("Yhteenvetobonus.txt", "w", encoding="utf-8") as f:
+                f.write("Raportti viikosta 41\n")
+                f.write("...\n")
+                f.write(Viikko41yht)
+                print("Raportti viikosta 41 luotu.")
+        elif viikko == "42":
+            with open("Yhteenvetobonus.txt", "w", encoding="utf-8") as f:
+                f.write("Raportti viikosta 42\n")
+                f.write("...\n")
+                f.write(Viikko42yht)
+                print("Raportti viikosta 42 luotu.")
+            ...
+        elif viikko == "43":
+           with open("Yhteenvetobonus.txt", "w", encoding="utf-8") as f:
+                f.write("Raportti viikosta 43\n")
+                f.write("...\n")
+                f.write(Viikko43yht)
+                print("Raportti viikosta 43 luotu.")
+                ...
+        else:
+            print("Tuntematon viikkonumero.")
+
+    else:
+        print("Virheellinen valinta.")
+
+
 
 
 if __name__ == "__main__":
